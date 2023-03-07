1da2f6481f84c21de84a43e400e052224745f3b8
darkinho777 <127205871+darkinho777@users.noreply.github.com>
2023-03-07 10:54:24 -0300

    Add files via upload

---
+++ tabuada.js
@@ -0,0 +1,10 @@
+function imprimirtabuada(n){
+    for (var i = 1; i <= 10; i++)  {
+        var linha = n + "*" + i + "*" + n * i;
+            alert("" + linha)
+                console.log(linha);}}
+            
+    for (var i = 1; i <=10; i++) {
+        imprimirtabuada(1)
+        alert("" + i)
+        console.log("");}
